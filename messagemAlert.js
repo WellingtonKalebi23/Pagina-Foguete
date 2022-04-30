f1605b46a699e908cc2b0b4739edf9746facf4cc
WellingtonKalebi <wellington_rdp@live.com>
2022-04-30 19:55:23 -0300

    ok

--- messagemAlert.js
+++ messagemAlert.js
@@ -1,7 +1,7 @@
 var btLogin = document.getElementById("alert");
 
 function dialogoAlert() {
-    alert("Partiu 3°Fase?")
+    alert("Partiu 3ºFase?")
 }
 
 btLogin.addEventListener("click", dialogoAlert)
